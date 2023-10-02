86266f6c845568ac02a60f835f66b045218613f1
Leo-Wolff <leokramer17@gmail.com>
2023-10-02 17:35:17 +0200

    Add animated background

--- static/script.js
+++ static/script.js
@@ -11,3 +11,49 @@ text.innerHTML = text.innerText
             }deg);" >${char}</span>`
     )
     .join("")
+
+const container = document.getElementById("fireflies-container")
+const fireflyTotal = 300
+
+createFirefly = () => {
+    const firefly = document.createElement("div")
+    firefly.classList.add("firefly")
+    container.appendChild(firefly)
+    console.log("firefly created")
+
+    positionFirefly(firefly)
+    animateFirefly(firefly)
+}
+
+positionFirefly = (element) => {
+    const randomX = Math.random() * window.innerWidth
+    const randomY = Math.random() * document.body.clientHeight
+    const randomSize = Math.random() * 10
+
+    element.style.left = randomX + "px"
+    element.style.top = randomY + "px"
+    element.style.width = randomSize + "px"
+    element.style.height = randomSize + "px"
+}
+
+animateFirefly = (firefly) => {
+    const randomX = Math.random() * window.innerWidth
+    const randomY = Math.random() * document.body.clientHeight
+    const duration = Math.random() * 10000 + 5000 // Random duration between 2 to 5 seconds
+
+    firefly.style.transition = `transform ${duration}ms linear`
+
+    setTimeout(() => {
+        firefly.style.transform = `translate(${
+            randomX - parseInt(firefly.style.left)
+        }px, ${randomY - parseInt(firefly.style.top)}px)`
+        setTimeout(() => {
+            container.removeChild(firefly) // Remove the firefly when the animation is done
+            createFirefly()
+        }, duration)
+    }, 0)
+}
+
+for (i = 0; i < fireflyTotal; i++) {
+    createFirefly()
+}
